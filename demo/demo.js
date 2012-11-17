d9bbca3454e9f62c4ff82155373a50ace50c17e0
Scott W. Elwood <McPants32@gmail.com>
2012-11-17 09:06:20 -0800

    Updated Demo JS

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,59 @@
+var $containers = $(".container");
+
+function renderChildren(placekitten) {
+  // Lets generate some child divs
+  $containers.children().remove();
+  $containers.each(function() {
+    for(i=0;i<15;i++) {
+      var $element = $("<div></div>"),
+          height = Math.floor(Math.random() * 200) + 100,
+          width = 200;
+      if(placekitten) {
+        var background = 'url("http://www.placekitten.com/'+width+'/'+height+'")';
+      } else {
+        var background = getRandomColor(),
+            $img = $('<img src="http://placehold.it/'+width+'x'+Math.floor(height / 2)+'" />');
+        $element.append($img);
+      }
+      $element.css({ background: background, height: height, width: width });
+      $(this).append($element);
+    }
+  });
+}
+renderChildren(false);
+
+// And now we can shapeshift!
+$(".filter").on("click", function(e) {
+  e.preventDefault();
+
+  if($(this).hasClass("no-drag-animate")) {
+    $containers.shapeshift({animatedOnDrag: false});
+  }
+  if($(this).hasClass("no-drag")) {
+    $containers.shapeshift({draggable: false});
+  }
+  if($(this).hasClass("drag")) {
+    $containers.shapeshift();
+  }
+  if($(this).hasClass("hide")) {
+    var $objects = $containers.children().filter(":visible"),
+        random = Math.round(Math.random()*7);
+    $objects.eq(random).hide();
+    $containers.shapeshift();
+  }
+  if($(this).hasClass("show")) {
+    var $objects = $containers.children().filter(":hidden"),
+        random = Math.round(Math.random()*3);
+    $objects.eq(random).show();
+    $containers.shapeshift();
+  }
+  if($(this).hasClass("placekittens")) {
+    renderChildren(true);
+    $containers.shapeshift();
+  }
+})
+$containers.shapeshift();
+
 function getRandomColor() {
   var letters = 'ABCDEF'.split('');
   var color = '#';
@@ -6,73 +62,3 @@ function getRandomColor() {
   }
   return color;
 }
-
-// Lets generate some child divs
-var $container = $(".container");
-$container.each(function() {
-  for(i=0;i<20;i++) {
-    var $element = $("<div></div>"),
-        height = Math.floor(Math.random() * 200) + 100,
-        width = 200,
-        color = getRandomColor(),
-        $img = $('<img src="http://placehold.it/'+width+'x'+Math.floor(height / 2)+'" />');
-    $element.css({
-      height: height,
-      background: color
-    });
-    $(this).append($element);
-    $element.append($img);
-  }
-})
-
-// And now we can shapeshift!
-$(".filter").on("click", function(e) {
-  e.preventDefault();
-
-  if($(this).hasClass("no-drag-animate")) {
-    $container.shapeshift({animatedOnDrag: false});
-  }
-  if($(this).hasClass("no-drag")) {
-    $container.shapeshift({draggable: false});
-  }
-  if($(this).hasClass("drag")) {
-    $container.shapeshift();
-  }
-  if($(this).hasClass("hide")) {
-    var $objects = $container.children().filter(":visible"),
-        random = Math.round(Math.random()*7);
-    $objects.eq(random).hide();
-    $(".container").shapeshift();
-  }
-  if($(this).hasClass("show")) {
-    var $objects = $container.children().filter(":hidden"),
-        random = Math.round(Math.random()*3);
-    $objects.eq(random).show();
-    $(".container").shapeshift();
-  }
-  if($(this).hasClass("placekittens")) {
-    $container.each(function() {
-      var $objects = $(this).children().filter(":visible");
-      $objects.each(function(i) {
-        var width = $(this).outerWidth(),
-            height = $(this).outerHeight(),
-            $img = $('<img src="http://www.placekitten.com/'+width+"/"+height+'" width="'+width+'" height="'+height+'" />');
-
-        $img.css({
-          left: $(this).position().left,
-          top: $(this).position().top,
-          position: "absolute"
-        })
-        $container.append($img);
-        $(this).remove();
-
-        if(i === $objects.length - 1) {
-          setTimeout(function() {
-            $(".container").shapeshift();
-          }, 300);
-        }
-      })
-    })
-  }
-})
-$(".container").shapeshift();
