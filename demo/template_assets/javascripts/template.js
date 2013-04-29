b1c7039396155bf6d883b6846f8d69428505b864
Scott Elwood <mcpants32@gmail.com>
2013-04-29 15:00:00 -0700

    initial issues push

--- demo/template_assets/javascripts/template.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  // Button group toggling
-  $(".btn-group.toggle button").on("click", function() {
-    console.log("!")
-    $(this).siblings(".active").removeClass("active")
-    $(this).addClass("active")
-  })
-})
