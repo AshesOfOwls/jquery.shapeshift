14ff660b48dbeb5d30d01226c04c7afce7e4c88b
Scott Elwood <mcpants32@gmail.com>
2013-05-16 11:58:34 -0700

    Updated to JSFilled examples instead of gists

--- demo/assets/javascripts/ember/helpers.js
+++ demo/assets/javascripts/ember/helpers.js
@@ -16,3 +16,7 @@ Ember.Handlebars.registerHelper('gist', function(gist_id) {
 
   return new Handlebars.SafeString('<div class="'+gist_id+'">'+gist_html+'</div>')
 })
+
+Ember.Handlebars.registerHelper('jsfiddle', function(fiddle_id) {
+  return new Handlebars.SafeString('<iframe width="100%" height="300" src="http://jsfiddle.net/McPants/'+fiddle_id+'/embedded/result,js,html,css" allowfullscreen="allowfullscreen" frameborder="0"></iframe>')
+})
