dfbc993807c967a3f64745b6e9036f000507b4d6
Scott Elwood <Scott@workwithopal.com>
2016-06-17 14:00:37 -0700

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-    "extends": "airbnb-base",
+  "extends": "airbnb-base",
 
-    "env": {
-        "browser": true
-    }
+  "env": {
+    "browser": true
+  },
+
+  "rules": {
+    "no-underscore-dangle": ["error", {
+      "allow": ["_name", "_defaults"]
+    }]
+  }
 };
